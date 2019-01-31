cbf8389c668b9085d24637ff41dfa1ee27c3642a
Pavneet Singh Saund <pav.saund@gmail.com>
2019-01-31 22:23:22 +0100

    Hooked up the CreateItemCommand and GetTaskListByListIdQuery

--- TodoTracking/Web/Features/todos.js
+++ TodoTracking/Web/Features/todos.js
@@ -2,16 +2,24 @@ import {ObserverLocator, observable, computedFrom} from 'aurelia-binding';
 import {TodoItem} from './todo-item';
 import _ from 'underscore';
 import { inject } from 'aurelia-framework';
+import { CreateItem } from "./TodoItem/CreateItem";
+import { CommandCoordinator } from "@dolittle/commands";
+import { Guid } from "@dolittle/core";
+import { QueryCoordinator } from "@dolittle/queries";
+import { GetTaskListByListId } from "./TodoItem/GetTaskListByListId";
+
 
 const STORAGE_NAME = 'todomvc-aurelia';
 const ENTER_KEY = 13;
 
-@inject(ObserverLocator)
+@inject(ObserverLocator, CommandCoordinator, QueryCoordinator)
 export class todos {
 	@observable items = [];
 	@observable filter = '';
 	@observable newTodoTitle = null;
 	@observable areAllChecked = false;
+
+	listId;
 
 	@computedFrom('items', 'filter')
 	get filteredItems() {
@@ -28,17 +36,33 @@ export class todos {
 	}
 
 	
-	constructor(observerLocator, storage = null) {	
+	constructor(observerLocator, commandCoordinator, queryCoordinator, storage = null) {	
 		this.observerLocator = observerLocator;
 		this.storage = storage || localStorage;
+		this.commandCoordinator = commandCoordinator;
+		this.queryCoordinator = queryCoordinator;
 	}
 	
 	activate(params) {
 		this.filter = params.filter;
+		// this.listId = Guid.create();
+		this.listId = "9778bad6-71b3-8a63-75cf-1d72cc8fc387" //Guid.create();
 	}
 	
 	bind(bindingContext,overrideContext) {
 		this.load();
+		let queryForTaskList = new GetTaskListByListId();
+				queryForTaskList.listId = this.listId;
+
+				this.queryCoordinator.execute(queryForTaskList).then(queryResult => {
+					console.log(queryResult);
+					if(queryResult.success){
+						const taskList = queryResult.items[0];
+						this.items = taskList.tasks.map(task => new TodoItem(task.text));
+						this.newTodoTitle = null;
+					}
+
+				});
 	}
 
 	onKeyUp(ev) {
@@ -53,12 +77,35 @@ export class todos {
 		title = title.trim();
 		if (title.length === 0) { return; }
 
-		const newTodoItem = new TodoItem(title);
-		this.observeItem(newTodoItem);
-		this.items.push(newTodoItem);
-		this.newTodoTitle = null;
-		this.updateAreAllCheckedState();
-		this.save();
+		// const newTodoItem = new TodoItem(title);
+		// this.observeItem(newTodoItem);
+		// this.items.push(newTodoItem);
+		// this.newTodoTitle = null;
+		// this.updateAreAllCheckedState();
+		// this.save();
+
+		let createItem = new CreateItem();
+		createItem.list = this.listId;
+		createItem.text = title;
+
+		this.commandCoordinator.handle(createItem).then(commandResult => {
+			console.log(commandResult);
+			if(commandResult.success){
+				let queryForTaskList = new GetTaskListByListId();
+				queryForTaskList.listId = this.listId;
+
+				this.queryCoordinator.execute(queryForTaskList).then(queryResult => {
+					console.log(queryResult);
+					if(queryResult.success){
+						const taskList = queryResult.items[0];
+						this.items = taskList.tasks.map(task => new TodoItem(task.text));
+						this.newTodoTitle = null;
+					}
+
+				});
+			}
+		})
+
 	}
 
 	observeItem(todoItem) {
