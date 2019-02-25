638bb28157ebe283702a409b9fd03571f83765f9
Tomas Ekeli <tomas.ekeli@gmail.com>
2019-02-25 08:46:28 +0100

    Generated files for the ClearList command
    These files should be committed, otherwise each build will give them
    a new id and you'll run into problems when cooperating across
    multiple machines.
    
    Very common to forget to commit these changes, I do it all the time.

---
+++ TodoTracking/Web/Features/TodoItem/ClearList.js
@@ -0,0 +1,15 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  This file is an automatically generated Command Proxy
+ *  
+ *--------------------------------------------------------------------------------------------*/
+import { Command } from  '@dolittle/commands';
+
+export class ClearList extends Command
+{
+    constructor() {
+        super();
+        this.type = '0cf809b9-18d8-4e3d-a26f-bef417090914';
+
+        this.list = '00000000-0000-0000-0000-000000000000';
+    }
+}
